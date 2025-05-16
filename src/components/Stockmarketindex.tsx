7d0d7ab27429b19110eeeef6516af62b6b65fa7e
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-16 11:31:30 +0200

    refactor and begin to add logic to stackmarketindex

---
+++ src/components/Stockmarketindex.tsx
@@ -0,0 +1,4 @@
+import { fetchQuote } from "../utils/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const StockMarketIndex = () => {};
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,10 @@
-const API_URL = "";
+const API_URL = "https://finnhub.io/api/v1/";
 
 const API_KEY = "d0jfa31r01ql09hs4cg0d0jfa31r01ql09hs4cgg";
 
 export const fetchQuote = async (symbol: string) => {
   const result = await fetch(
-    `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${API_KEY}`
+    `${API_URL}quote?symbol=${symbol}&token=${API_KEY}`
   );
   return result.json;
 };
