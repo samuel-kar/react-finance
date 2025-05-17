d08e0c74ac68224efa72c2e79715534e0984caf9
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-17 11:53:13 +0200

    add calc for monthly compound interest

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,3 +8,18 @@ export const fetchQuote = async (symbol: string) => {
   );
   return result.json();
 };
+
+export const monthlyCompoundInterestCalculator = (
+  startValue: number,
+  annualRate: number,
+  years: number,
+  monthlyInput: number
+): number => {
+  const monthlyRate = annualRate / 12 / 100;
+  const months = years * 12;
+
+  return (
+    startValue * Math.pow(1 + monthlyRate, months) +
+    (monthlyInput * (Math.pow(1 + monthlyRate, months) - 1)) / monthlyRate
+  );
+};
