bd359ab54893352409e1bda7dd2fcd1b75f6ac4c
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-17 21:58:27 +0200

    try to implement tailwind but fail

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,27 @@ import { Link } from "@tanstack/react-router";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 flex gap-4">
-      <Link to="/" className="text-white hover:text-gray-300">
-        Home
-      </Link>
-      <Link to="/calculator" className="text-white hover:underline">
-        Calculator
-      </Link>
-      <Link to="/search" className="text-white hover:underline">
-        Search stock
-      </Link>
-      <Link to="/about" className="text-white hover:underline">
-        About
-      </Link>
+    <nav className="bg-gray-800 text-white p-4 flex flex-row gap-10 items-center">
+      <div>
+        <Link to="/" className="block text-white hover:text-gray-300">
+          Home
+        </Link>
+      </div>
+      <div>
+        <Link to="/calculator" className="block text-white hover:underline">
+          Calculator
+        </Link>
+      </div>
+      <div>
+        <Link to="/search" className="block text-white hover:underline">
+          Search stock
+        </Link>
+      </div>
+      <div>
+        <Link to="/about" className="block text-white hover:underline">
+          About
+        </Link>
+      </div>
     </nav>
   );
 };
