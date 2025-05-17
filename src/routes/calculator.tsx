ad92f445766704d2f98c7d2a9b03e50f4ea7d3cc
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-17 20:01:26 +0200

    add component to route

--- src/routes/calculator.tsx
+++ src/routes/calculator.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Savingcalculator } from "@/components/Savingcalculator";
 
 export const Route = createFileRoute("/calculator")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/calculator"!</div>;
+  return <Savingcalculator />;
 }
