4f90b1f7f7998d1f2c34fc91540d3da9f23bb8cf
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-19 09:40:50 +0200

    clean

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,21 @@ import { Link } from "@tanstack/react-router";
 export const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white p-4 flex flex-row gap-10 items-center">
-      <div>
-        <Link to="/" className="block text-white hover:text-gray-300">
-          Home
-        </Link>
-      </div>
-      <div>
-        <Link to="/calculator" className="block text-white hover:underline">
-          Calculator
-        </Link>
-      </div>
-      <div>
-        <Link to="/search" className="block text-white hover:underline">
-          Search stock
-        </Link>
-      </div>
-      <div>
-        <Link to="/about" className="block text-white hover:underline">
-          About
-        </Link>
-      </div>
+      <Link to="/" className="block text-white hover:text-gray-300">
+        Home
+      </Link>
+
+      <Link to="/calculator" className="block text-white hover:underline">
+        Calculator
+      </Link>
+
+      <Link to="/search" className="block text-white hover:underline">
+        Search stock
+      </Link>
+
+      <Link to="/about" className="block text-white hover:underline">
+        About
+      </Link>
     </nav>
   );
 };
--- src/routes/calculator.tsx
+++ src/routes/calculator.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Savingcalculator } from "@/components/Savingcalculator";
+import "../App.css";
 
 export const Route = createFileRoute("/calculator")({
   component: RouteComponent,
