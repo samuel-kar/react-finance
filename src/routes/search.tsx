7b8d944ea49ab1357951a86b97b55072a91829ff
samuel-kar <samuel.karlhager@hotmail.com>
2025-05-19 13:10:26 +0200

    implement route again

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,16 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { StockMarketIndex } from "@/components/Stockmarketindex";
+import { SearchBar } from "@/components/Searchbar";
 
 export const Route = createFileRoute("/search")({
   component: RouteComponent,
+  validateSearch: (search) => ({
+    symbol: typeof search.symbol === "string" ? search.symbol : undefined,
+  }),
 });
 
 function RouteComponent() {
+  const { symbol } = Route.useSearch();
+
   return (
-    <>
-      <div className="flex justify-center mt-20 px-4">
-        <StockMarketIndex symbol="MSFT" />
-      </div>
-    </>
+    <div className="flex flex-col items-center mt-20 px-4 space-y-8">
+      <SearchBar />
+      {symbol && <StockMarketIndex symbol={symbol} />}
+    </div>
   );
 }
